fa0b8ad5ce138babec2502164cf60f38f17aef70
Sergey Kruglov <serega-kolpino@inbox.ru>
2023-09-25 00:24:34 +0300

    feat: finished task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minValue = 0
+        this.maxValue = 0
+        this.res = 0
+    }
 
     setRange(min, max) {
-
+        this.minValue = min
+        this.maxValue = max
     }
 
     guess() {
-
+        this.res = Math.round((this.minValue + this.maxValue) / 2);
+        return this.res;
     }
 
     lower() {
-
+        return this.maxValue = this.res
     }
 
     greater() {
-
+        return this.minValue = this.res
     }
 }
 
